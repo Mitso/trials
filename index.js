b05439811e9f5f9f472e9339894b3ae346bf3089
Mitso Qalaba <mqalaba@gmail.com>
2020-08-14 19:04:44 +0200

    Enable node express

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 "use strict";
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = process.env.PORT || 3000
 
-console.log('Welcome to mock site');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname + '/profile.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
